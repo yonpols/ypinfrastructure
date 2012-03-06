d17c53fab8d7aedf96ad75c4f32fa6b30ed6c2b4
Juan Pablo Marzetti <yonpols@gmail.com>
2012-03-06 11:36:11 -0300

    Changed the default cloning dest path

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ "$1" == "global" ]; then
     DEST_PATH=/usr/local/ypi
 fi;
 
-if mkdir -p $DEST_PATH && cd $DEST_PATH && git clone git://github.com/yonpols/ypinfrastructure.git; then
+if mkdir -p $DEST_PATH && git clone git://github.com/yonpols/ypinfrastructure.git $DEST_PATH; then
     echo 'export PATH=$PATH:$DEST_PATH/bin' >> ~/.bashrc
     source ~/.bashrc
     echo "YPInfrastructure installed correctly"
